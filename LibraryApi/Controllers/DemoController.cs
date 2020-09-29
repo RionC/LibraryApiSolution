c3de2f714d75c09dbfa7bc70e1bf9d13ae81770c
RionC <67234765+RionC@users.noreply.github.com>
2020-09-29 12:26:18 -0400

    Before lunch complete

--- LibraryApi/Controllers/DemoController.cs
+++ LibraryApi/Controllers/DemoController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,10 +11,17 @@ namespace LibraryApi.Controllers
     {
         // Route Params
         // GET /employees/19
-        [HttpGet("/employees/{employeeId:int}")]
+        [HttpGet("/employees/{employeeId:int}", Name ="employees#get")]
         public ActionResult LookupEmployee([FromRoute] int employeeId)
         {
-            return Ok($"Finding employee { employeeId }");
+            var response = new GetEmployeeDetailsResponse
+            {
+                Id = employeeId,
+                Name = "Joe Smith",
+                Department = "DEV",
+                StartingSalary = 50000
+            };
+            return Ok(response);
         }
 
         // GET /blogs/2020/09/18
@@ -22,6 +30,61 @@ namespace LibraryApi.Controllers
         {
             return Ok($"Getting blogs for {year}/{month}/{day}");
         }
+
         // Query Strings
+        // GET /employees?dept=DEV
+        [HttpGet("/employees")]
+        public ActionResult GetEmployees([FromQuery]string department = "All", [FromQuery] decimal minSalary = 0)
+        {
+            return Ok($"Returning all employees from {department} with a minimum salary of {minSalary:c}");
+        }
+
+        // Headers
+        [HttpGet("/whoami")]
+        public ActionResult ShowUserAgent([FromHeader(Name ="User-Agent")]string userAgent)
+        {
+            return Ok($"I see you are running {userAgent}");
+        }
+
+        // Entities
+        [HttpPost("/employees")]
+        public ActionResult Hire([FromBody] PostEmployeeCreate employeeToHire)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var response = new GetEmployeeDetailsResponse
+            {
+                Id = new Random().Next(50, 10000),
+                Name = employeeToHire.Name,
+                Department = employeeToHire.Department,
+                StartingSalary = employeeToHire.StartingSalary
+            };
+            return CreatedAtRoute("employees#get", new { employeeId = response.Id }, response);
+        }
+    }
+
+    public class PostEmployeeCreate
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Range(10000, 200000)]
+        public decimal StartingSalary { get; set; }
+    }
+
+    public class GetEmployeeDetailsResponse
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Range(10000, 200000)]
+        public decimal StartingSalary { get; set; }
     }
 }
